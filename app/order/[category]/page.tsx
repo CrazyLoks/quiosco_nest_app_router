015ce105121e8122f36a28df7b5a8fb5c2f142d9
Crazy Loks <adrianmendez2001@gmail.com>
2024-08-27 19:17:00 -0600

    Se agregan los iconos y se agrega el routing dinámico en orders con la carpeta [category]

---
+++ app/order/[category]/page.tsx
@@ -0,0 +1,6 @@
+//* Con la carpeta llamada asi, entre corchetes, va a ser dinámica, por cada categoria vamos a tener este archivo, una page
+export default function OrderPage({params} : { params: { category : string }}) {
+  return (
+    <div>OrderPage</div>
+  )
+}
