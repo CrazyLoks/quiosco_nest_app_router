c26ca0b2d5d052daee9bc5dfd8d1f3d046f0a9d9
Crazy Loks <adrianmendez2001@gmail.com>
2024-08-27 18:39:06 -0600

    Se consultan las categorias desde el lado de Next.js

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,5 +1,13 @@
+import { prisma } from '@/src/lib/prisma';
 
-export default function OrderSidebar() {
+async function getCategories() {
+  return await prisma.category.findMany(); // Nos traemos todas las categorías
+}
+
+export default async function OrderSidebar() { // Next.js si soporta componentes asincronos
+
+  const categories = await getCategories();
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
         OrderSidebar
