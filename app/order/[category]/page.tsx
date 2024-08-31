a7cfe96f0f83887ed19e81595a92ce5cb480d9f1
Crazy Loks <adrianmendez2001@gmail.com>
2024-08-30 18:40:41 -0600

    Se agrega función para traer los productos de la categoría seleccionada

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,5 +1,25 @@
 //* Con la carpeta llamada asi, entre corchetes, va a ser dinámica, por cada categoria vamos a tener este archivo, una page
-export default function OrderPage({params} : { params: { category : string }}) {
+
+import { prisma } from "@/src/lib/prisma"
+
+
+async function getProducts( category: string ) {
+  const products = await prisma.product.findMany({ // buscamos todos los productos cuya categoria sea igual a la que estamos pasando
+    where: {
+      category: {
+        slug: category // que el slug sea igual a la category que estamos pasando
+      }
+    }
+  })
+
+  return products;
+}
+
+export default async function OrderPage({params} : { params: { category : string }}) { // Next permite componentes asyncronos
+
+  const products = await getProducts(params.category);
+  console.log(products);
+  
   return (
     <div>OrderPage</div>
   )
