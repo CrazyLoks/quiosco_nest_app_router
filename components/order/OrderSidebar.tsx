cabfddd9f4e3273917e4ae4a2c55451d2ce0e268
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-09 20:15:59 -0600

    Se agrega el logo en layouts y se crea el componente para las ordenes

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,5 +1,6 @@
 import { prisma } from '@/src/lib/prisma';
 import CategoryIcon from '../ui/CategoryIcon';
+import Logo from '../ui/Logo';
 
 async function getCategories() {
   return await prisma.category.findMany(); // Nos traemos todas las categorías
@@ -11,7 +12,7 @@ export default async function OrderSidebar() { // Next.js si soporta componentes
 
   return (
     <aside className="md:w-72 md:h-screen bg-white">
-        
+        <Logo />
         <nav className='mt-10'>
           {categories.map(category => (
             <CategoryIcon 
