f08d8f72ae1c5284e01073600ce444b03de631ed
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-09 21:28:24 -0600

    Se agrega action para completar la orden

---
+++ actions/complete-order-action.ts
@@ -0,0 +1,5 @@
+"use server" // aunque sea un componente de servidor, en los actions hay que poner esto
+
+export async function completeOrder() {
+    console.log('desde');
+}
--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,3 +1,4 @@
+import { completeOrder } from "@/actions/complete-order-action"
 import { formatCurrency } from "@/src/lib/utils"
 import { OrderWithProducts } from "@/src/types"
 
@@ -6,6 +7,8 @@ type OrderCardProps = {
 }
 
 export default function OrderCard({ order } : OrderCardProps) {
+
+
 
     return (
         <section
@@ -32,7 +35,7 @@ export default function OrderCard({ order } : OrderCardProps) {
                 </div>
             </dl>
 
-            <form>
+            <form action={completeOrder}>
                 <input
                     type="submit"
                     className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
