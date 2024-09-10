49227865f271e6f8b446bad002a398372ff9ac3a
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-09 21:11:27 -0600

    Se agrega componente para cada orden

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
-import { Product } from "@prisma/client";
+import { Order, OrderProducts, Product } from "@prisma/client";
 
 export type OrderItem = Pick<Product, 'id' | 'name' | 'price' > & { // Va a tener esos 3 atributos más los otros 2 que estamos configurando
     quantity: number
     subtotal: number
+}
+
+export type OrderWithProducts = Order & {
+    orderProducts: (OrderProducts & {
+        product: Product
+    })[]
 }
