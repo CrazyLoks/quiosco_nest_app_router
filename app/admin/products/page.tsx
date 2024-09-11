a5fa9a4166f5f39e158e024b384b682d2bda3cc6
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-10 18:46:11 -0600

    Se hacen modificaciones para el paginador

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,20 @@
+import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import page from './new/page';
 
-async function getProducts() {
+async function productCount() {
+  return await prisma.product.count(); // Retorna cuantos productos hay en la DB
+}
+
+async function getProducts(page: number, pageSize: number) {
+
+  const skip = (page - 1) * pageSize; // Calculamos en que página estamos
+
   const products = await prisma.product.findMany({ // Nos traemos todos los productos
+    take: pageSize, // take es cuantos registros va a tomar
+    skip, // skip es cuantosa registros se va a 'saltar'
     include: {
       category: true
     }
@@ -12,10 +23,14 @@ async function getProducts() {
   return products;
 }
 
-export default async function ProductsPage() {
+export default async function ProductsPage({searchParams} : { searchParams: {page: string}}) { // searchParams es interno de Next, es para recuperar los parámetros de la URL
 
-  const products = await getProducts();
-  console.log(products);
+  const page = +searchParams.page || 1; // si no existe el parámetro page, se asignará 1
+  const pageSize = 10;
+
+  const productsData = getProducts(page, pageSize);
+  const totalProductsData = productCount();
+  const [ products, totalProducts ] = await Promise.all([productsData, totalProductsData]); // Se hacen ambas consultas al mismo tiempo porque ninguna depende de la otra
 
   return (
     <>
@@ -23,6 +38,10 @@ export default async function ProductsPage() {
 
         <ProductsTable 
           products={products}
+        />
+
+        <ProductsPagination 
+          page={page}
         />
     </>
   )
