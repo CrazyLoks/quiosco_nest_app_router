deda3715077d41329ca3ae347de0849764264fcd
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-10 18:02:48 -0600

    Se agrega tabla para ver todos los productos

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,25 @@
+import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function ProductsPage() {
+async function getProducts() {
+  const products = await prisma.product.findMany(); // Nos traemos todos los productos
+
+  return products;
+}
+
+export default async function ProductsPage() {
+
+  const products = await getProducts();
+  console.log(products);
+
   return (
     <>
         <Heading>Administrar Productos</Heading>
+
+        <ProductsTable 
+          products={products}
+        />
     </>
   )
 }
