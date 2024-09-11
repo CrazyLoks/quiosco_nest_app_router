8f079b63c949825875d9b96276626ecdeacdb26f
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-10 20:19:14 -0600

    Se agrega input para buscar un producto

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -4,6 +4,8 @@ import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 import page from './new/page';
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import ProductSearchForm from "@/components/products/ProductSearchForm";
 
 async function productCount() {
   return await prisma.product.count(); // Retorna cuantos productos hay en la DB
@@ -41,6 +43,17 @@ export default async function ProductsPage({searchParams} : { searchParams: {pag
   return (
     <>
         <Heading>Administrar Productos</Heading>
+
+        <div className="flex flex-col lg:flex-row lg:justify-between gap-5">
+          <Link
+            href={'/admin/products/new'}
+            className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+          >
+            Crear Producto
+          </Link>
+
+          <ProductSearchForm />
+        </div>
 
         <ProductsTable 
           products={products}
