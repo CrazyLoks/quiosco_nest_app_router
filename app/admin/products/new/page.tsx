1fd0b1eac598aa15cbea887f17524547cbfbb97f
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-12 20:16:47 -0600

    Se agrega composicion (componente de servidor > cliente > servidor)

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,4 +1,5 @@
 import AddProductForm from "@/components/products/AddProductForm";
+import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/components/ui/Heading";
 
 export default function CreateProductPage() {
@@ -6,7 +7,9 @@ export default function CreateProductPage() {
     <>
       <Heading>Nuevo Producto</Heading>
 
-      <AddProductForm />
+      <AddProductForm>
+        <ProductForm /> {/*componente del servidor, se manda como children porque no se puede tener un componente de servidor, despues de cliente y despues de servidor, pero de esta manera si se puede, detecta que hay un componente del servidor y registra o guarda ese espacio para el componente (composicion) */}
+      </AddProductForm>
     </>
   )
 }
