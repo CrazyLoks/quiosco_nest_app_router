6ef4a6c372ece870cab197ea66546b7ba0276ec8
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-17 19:35:04 -0600

    Se agrega funcion para traer imagenes de manera dinamica, dependiendo su ubicacion

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { formatCurrency } from "@/src/lib/utils"
+import { formatCurrency, getImagePath } from "@/src/lib/utils"
 import { Product } from "@prisma/client"
 import Image from "next/image"
 import AddProductButton from "./AddProductButton"
@@ -9,26 +9,28 @@ type ProductCardProps = {
 }
 
 export default function ProductCard({product} : ProductCardProps) {
-  return (
-    <div className="border bg-white">
+    
+    const imagePath = getImagePath(product.image);
+    return (
+        <div className="border bg-white">
 
-        <Image 
-            width={400}
-            height={500}
-            src={`/products/${product.image}.jpg`}
-            alt={`Imagen platillo ${product.name}`}
-            quality={75} // Quality es la calidad de la imagen, el default (si no lo ponemos) es 75, va de 1 a 100, siendo 100 la mejor calidad
-        />
+            <Image 
+                width={400}
+                height={500}
+                src={imagePath}
+                alt={`Imagen platillo ${product.name}`}
+                quality={75} // Quality es la calidad de la imagen, el default (si no lo ponemos) es 75, va de 1 a 100, siendo 100 la mejor calidad
+            />
 
-        <div className="p-5">
-            <h3 className="text-2xl font-bold">{product.name}</h3>
-            <p className="mt-5 font-black text-4xl text-amber-500">
-                {formatCurrency(product.price)}
-            </p>
-            <AddProductButton // Ejecutamos solamente e botón del dalo del cliente, el resto de este componente es del servidor
-                product={product}
-            /> 
+            <div className="p-5">
+                <h3 className="text-2xl font-bold">{product.name}</h3>
+                <p className="mt-5 font-black text-4xl text-amber-500">
+                    {formatCurrency(product.price)}
+                </p>
+                <AddProductButton // Ejecutamos solamente e botón del dalo del cliente, el resto de este componente es del servidor
+                    product={product}
+                /> 
+            </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,3 +4,12 @@ export function formatCurrency(amount: number) {
         currency: 'USD'
     }).format(amount)
 }
+
+export function getImagePath(imagePath: string) { // va a recibir la url de la imagen
+    const cloudinaryBaseUrl = 'https://res.cloudinary.com' // asi empiezan las que están guardadas en cloudinary
+    if (imagePath.startsWith(cloudinaryBaseUrl)) {
+        return imagePath;
+    } else {
+        return `/products/${imagePath}.jpg`
+    }
+}
