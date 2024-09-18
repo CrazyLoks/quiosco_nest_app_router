73b736fc3f7587818771fd17a7bff76c0ff976f5
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-17 21:33:20 -0600

    Se agrega componente para ver las ordenes listas y la API de las mismas

---
+++ app/orders/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Logo from "@/components/ui/Logo";
+
+
+export default function page() {
+  return (
+    <>
+        <h1 className="text-center mt-20 text-6xl font-black">Ordenes Listas</h1>
+
+        <Logo />
+    </>
+  )
+}
