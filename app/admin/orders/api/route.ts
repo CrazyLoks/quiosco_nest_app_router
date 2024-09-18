52ff0e852ffcd267a37eaf11d0f2a64a0206dee0
Crazy Loks <adrianmendez2001@gmail.com>
2024-09-17 22:21:48 -0600

    Se hacen dinamicas las API para que no se queden cacheadas

--- app/admin/orders/api/route.ts
+++ app/admin/orders/api/route.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/src/lib/prisma"
 
+export const dynamic = 'force-dynamic'; // para que las API sean dinamicas y no se queden cacheadas
+
 export async function GET() { // Se crea en automatico una ruta GET
 
     const orders = await prisma.order.findMany({
